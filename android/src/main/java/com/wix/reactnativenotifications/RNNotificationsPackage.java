7d955ac84db596246db6a3d4570f3f42b15360ae
Deniss Borisovs <deniss.borisovs@gmail.com>
2017-10-25 18:02:35 +0200

    fix: fix for RN version 0.47

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -3,7 +3,6 @@ package com.wix.reactnativenotifications;
 import android.app.Application;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -13,8 +12,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNNotificationsPackage implements ReactPackage {
-
-
     final Application mApplication;
 
     public RNNotificationsPackage(Application application) {
@@ -24,11 +21,6 @@ public class RNNotificationsPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RNNotificationsModule(mApplication, reactContext));
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
